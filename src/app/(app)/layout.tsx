15de497367ccc90844573c06f71c0aeed7e70302
Peter Abah <peterabah.ob@gmail.com>
2024-09-11 10:39:10 +0100

    chore: initial setup

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"], display: "swap", variable: "--font-open-sans" });
+
+export const metadata: Metadata = {
+  title: "PixelBind",
+  description:
+    "PixelBind is a simple and powerful web tool that lets you quickly convert your images into high-quality PDFs. Easily upload, arrange, and customize your photos, then generate a downloadable PDF in just a few clicks. Perfect for organizing images into professional documents with ease. No sign-up required!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${openSans.variable}`}>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
