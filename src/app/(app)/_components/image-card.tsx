02174e746a991d2bc98d828278f553326b1622c1
Peter Abah <peterabah.ob@gmail.com>
2024-09-15 19:13:13 +0100

    feat: add removing and dragging images

--- src/app/(app)/_components/image-card.tsx
+++ src/app/(app)/_components/image-card.tsx
@@ -1,19 +1,63 @@
 import { PDFImage } from "@/lib/types";
+import { DraggableAttributes } from "@dnd-kit/core";
+import { SyntheticListenerMap } from "@dnd-kit/core/dist/hooks/utilities";
+import { useSortable } from "@dnd-kit/sortable";
+import { X } from "lucide-react";
 import Image from "next/image";
+import { CSSProperties, forwardRef } from "react";
 
 interface ImageCardProps {
   image: PDFImage;
-//   onRemove: (id: PDFImage["id"]) => void;
+  onRemove: (id: PDFImage["id"]) => void;
+  _internal?: {
+    style?: CSSProperties;
+    attributes?: DraggableAttributes;
+    listeners?: SyntheticListenerMap;
+  };
 }
-function ImageCard({ image }: ImageCardProps) {
+export function DraggableImageCard(props: ImageCardProps) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: props.image.id,
+  });
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+        transition,
+      }
+    : undefined;
+  return <ImageCard ref={setNodeRef} {...props} _internal={{ attributes, listeners, style }} />;
+}
+
+export const ImageCard = forwardRef<HTMLDivElement, ImageCardProps>(function ImageCard(
+  { image, onRemove, _internal },
+  ref
+) {
   return (
     <div
-      key={image.id}
-      className="bg-neutral-50 border rounded-md aspect-square inline-block relative"
+      ref={ref}
+      className="bg-neutral-50 border rounded-md inline-block p-2 min-w-[200px] touch-manipulation"
+      {..._internal?.attributes}
+      {..._internal?.listeners}
+      style={_internal?.style}
     >
-      <Image src={image.dataURL} alt="" fill className="object-contain" />
+      <div className="flex justify-end">
+        <button
+          className="p-2 rounded-full hover:bg-neutral-200 top-0 right-0 bg-neutral-50"
+          title="Remove image"
+          type="button"
+          onClick={() => onRemove(image.id)}
+        >
+          <X className="w-4 h-4" />
+          <span className="sr-only">Remove image</span>
+        </button>
+      </div>
+
+      <div className="relative aspect-square">
+        <Image src={image.dataURL} alt="" fill className="object-contain" />
+      </div>
     </div>
   );
-}
+});
 
 export default ImageCard;
--- src/app/(app)/_components/tool.tsx
+++ src/app/(app)/_components/tool.tsx
@@ -1,20 +1,39 @@
 "use client";
 
 import { Label } from "@/components/ui/label";
-import { PDFImage } from "@/lib/types";
-import { ImagePlus } from "lucide-react";
-
+import { generatePDF } from "@/lib/jspdf";
 import { transformFiles } from "@/lib/pdfimage";
-
+import { PDFImage } from "@/lib/types";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  closestCenter,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import { ImagePlus } from "lucide-react";
 import { ChangeEvent, useState } from "react";
 import EmptyState from "./empty-state";
-import ImageCard from "./image-card";
+import { DraggableImageCard, ImageCard } from "./image-card";
 import PDFOptions from "./pdf-options";
-import { generatePDF } from "@/lib/jspdf";
 
 function Tool() {
   const [images, setImages] = useState<PDFImage[]>([]);
   const [modalContainer, setModalContainer] = useState<HTMLElement | null>(null);
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+    useSensor(TouchSensor)
+  );
+  const [activeImageID, setActiveImageID] = useState<string | null>(null);
   const handleSelectImages = async (files: File[]) => {
     setImages(await transformFiles(files));
   };
@@ -37,13 +56,55 @@ function Tool() {
     return <EmptyState onSelectImages={handleSelectImages} />;
   }
 
+  const removeImage = (id: PDFImage["id"]) => {
+    setImages((images) => images.filter((img) => img.id !== id));
+  };
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setImages((images) => {
+        const oldIndex = images.findIndex(({ id }) => id === active.id);
+        const newIndex = images.findIndex(({ id }) => id === over?.id);
+
+        return arrayMove(images, oldIndex, newIndex);
+      });
+    }
+
+    setActiveImageID(null);
+  }
+
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+
+    setActiveImageID(active.id as string);
+  }
+
   return (
     <main className="grow relative" ref={setModalContainer} id="options-modal-container">
       <div className="absolute inset-0">
         <div className="md:w-[calc(100vw_-_20rem)] max-h-full scrollbar-thin overflow-y-auto grow py-6 px-8 gap-6 justify-center grid grid-cols-[repeat(auto-fit,200px)]">
-          {images.map((img) => (
-            <ImageCard image={img} key={img.id} />
-          ))}
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+            onDragStart={handleDragStart}
+          >
+            <SortableContext items={images}>
+              {images.map((img) => (
+                <DraggableImageCard image={img} key={img.id} onRemove={removeImage} />
+              ))}
+            </SortableContext>
+            <DragOverlay>
+              {activeImageID ? (
+                <ImageCard
+                  image={images.find((image) => image.id === activeImageID)!}
+                  onRemove={removeImage}
+                />
+              ) : null}
+            </DragOverlay>
+          </DndContext>
           <Label
             id="add-more"
             className="aspect-square rounded-md bg-neutral-50 hover:bg-neutral-100 border flex flex-col justify-center items-center gap-2 cursor-pointer focus-within:border-neutral-500"
