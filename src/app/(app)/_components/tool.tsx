e22aef1f35151bf16cc87ade001fb599b87483bf
Peter Abah <peterabah.ob@gmail.com>
2024-09-15 06:41:57 +0100

    feat: make options sidebar responsive

--- src/app/(app)/_components/tool.tsx
+++ src/app/(app)/_components/tool.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -13,8 +14,17 @@ import {
 import { Switch } from "@/components/ui/switch";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { ImageOptions, jsPDF, type ImageFormat } from "jspdf";
-import { DownloadIcon, ImagePlus, ImageUp, LoaderCircle, RefreshCwIcon } from "lucide-react";
+import {
+  DownloadIcon,
+  ImagePlus,
+  ImageUp,
+  LoaderCircle,
+  RefreshCwIcon,
+  Settings2,
+  X,
+} from "lucide-react";
 import { nanoid } from "nanoid";
 import Image from "next/image";
 import { ChangeEvent, DragEvent, useState } from "react";
@@ -107,7 +117,7 @@ const getPageOptions = (image: PDFImage, options: IPDFOptions) => {
 };
 function Tool() {
   const [images, setImages] = useState<PDFImage[]>([]);
-
+  const [modalContainer, setModalContainer] = useState<HTMLElement | null>(null);
   const handleSelectImages = async (files: File[]) => {
     setImages(await transformFiles(files));
   };
@@ -157,9 +167,9 @@ function Tool() {
   }
 
   return (
-    <main className="grow relative">
+    <main className="grow relative" ref={setModalContainer} id="options-modal-container">
       <div className="absolute inset-0">
-        <div className="w-[calc(100vw_-_20rem)] max-h-full scrollbar-thin overflow-y-auto grow py-6 px-8 gap-6 justify-center grid grid-cols-[repeat(auto-fit,200px)]">
+        <div className="md:w-[calc(100vw_-_20rem)] max-h-full scrollbar-thin overflow-y-auto grow py-6 px-8 gap-6 justify-center grid grid-cols-[repeat(auto-fit,200px)]">
           {images.map((img) => (
             <div
               key={img.id}
@@ -186,7 +196,7 @@ function Tool() {
         </div>
       </div>
 
-      <PDFOptions onGenerate={generatePDF} onReset={reset} />
+      <PDFOptions onGenerate={generatePDF} onReset={reset} container={modalContainer} />
     </main>
   );
 }
@@ -223,9 +233,10 @@ type IPDFOptions = OptionsSchema;
 interface PDFOptionsProps {
   onGenerate: (options: IPDFOptions) => void;
   onReset: () => void;
+  container: HTMLElement | null;
 }
 
-function PDFOptions({ onGenerate, onReset }: PDFOptionsProps) {
+function PDFOptions({ onGenerate, onReset, container }: PDFOptionsProps) {
   const {
     register,
     control,
@@ -256,85 +267,89 @@ function PDFOptions({ onGenerate, onReset }: PDFOptionsProps) {
   };
 
   return (
-    <div className="w-80 scrollbar-thin overflow-y-auto border-l z-10 py-6 px-4 h-full shrink-0 absolute top-0 right-0">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-between gap-4">
-        <h2 className="text-lg font-semibold mb-6 sr-only">PDF Options</h2>
+    <>
+      <div className="hidden md:w-80 w-full max-w-[90%] scrollbar-thin overflow-y-auto border border-l z-10 py-6 px-4 h-full shrink-0 bg-background absolute top-0 right-0 md:flex flex-col">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col justify-between gap-4 grow"
+        >
+          <h2 className="text-lg font-semibold mb-6 sr-only">PDF Options</h2>
 
-        <div className="flex flex-col gap-4 grow">
-          <div className="space-y-2">
-            <Label htmlFor="page-size">Page Size</Label>
-            <Controller
-              control={control}
-              name="size"
-              render={({ field }) => (
-                <Select onValueChange={field.onChange} {...field}>
-                  <SelectTrigger id="page-size">
-                    <SelectValue placeholder="Select page size" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="a4">A4</SelectItem>
-                    <SelectItem value="letter">Letter</SelectItem>
-                    <SelectItem value="legal">Legal</SelectItem>
-                    <SelectItem value="fit">Fit (Same size as image)</SelectItem>
-                  </SelectContent>
-                </Select>
-              )}
-            />
-          </div>
+          <div className="flex flex-col gap-4 grow">
+            <div className="space-y-2">
+              <Label htmlFor="page-size">Page Size</Label>
+              <Controller
+                control={control}
+                name="size"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} {...field}>
+                    <SelectTrigger id="page-size">
+                      <SelectValue placeholder="Select page size" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="a4">A4</SelectItem>
+                      <SelectItem value="letter">Letter</SelectItem>
+                      <SelectItem value="legal">Legal</SelectItem>
+                      <SelectItem value="fit">Fit (Same size as image)</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="page-size">Margin</Label>
+            <div className="space-y-2">
+              <Label htmlFor="page-size">Margin</Label>
 
-            <Controller
-              control={control}
-              name="margin"
-              render={({ field }) => (
-                <Select onValueChange={field.onChange} {...field}>
-                  <SelectTrigger id="page-size">
-                    <SelectValue placeholder="Select page margin" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="none">None</SelectItem>
-                    <SelectItem value="small">Small</SelectItem>
-                    <SelectItem value="normal">Normal</SelectItem>
-                    <SelectItem value="large">Big</SelectItem>
-                  </SelectContent>
-                </Select>
-              )}
-            />
-          </div>
+              <Controller
+                control={control}
+                name="margin"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} {...field}>
+                    <SelectTrigger id="page-size">
+                      <SelectValue placeholder="Select page margin" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="none">None</SelectItem>
+                      <SelectItem value="small">Small</SelectItem>
+                      <SelectItem value="normal">Normal</SelectItem>
+                      <SelectItem value="large">Big</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="orientation">Orientation</Label>
-            <Controller
-              control={control}
-              name="orientation"
-              render={({ field }) => (
-                <Select onValueChange={field.onChange} {...field}>
-                  <SelectTrigger id="orientation">
-                    <SelectValue placeholder="Select orientation" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="portrait">Portrait</SelectItem>
-                    <SelectItem value="landscape">Landscape</SelectItem>
-                  </SelectContent>
-                </Select>
-              )}
-            />
-          </div>
+            <div className="space-y-2">
+              <Label htmlFor="orientation">Orientation</Label>
+              <Controller
+                control={control}
+                name="orientation"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange} {...field}>
+                    <SelectTrigger id="orientation">
+                      <SelectValue placeholder="Select orientation" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="portrait">Portrait</SelectItem>
+                      <SelectItem value="landscape">Landscape</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
 
-          <div className="flex items-center justify-between">
-            <Label htmlFor="compress">Compress PDF</Label>
-            <Controller
-              control={control}
-              name="compress"
-              render={({ field }) => (
-                <Switch checked={field.value} onCheckedChange={field.onChange} id="compress" />
-              )}
-            />
-          </div>
+            <div className="flex items-center justify-between">
+              <Label htmlFor="compress">Compress PDF</Label>
+              <Controller
+                control={control}
+                name="compress"
+                render={({ field }) => (
+                  <Switch checked={field.value} onCheckedChange={field.onChange} id="compress" />
+                )}
+              />
+            </div>
 
-          {/* <div className="space-y-2">
+            {/* <div className="space-y-2">
                 <Label htmlFor="quality">Image Quality</Label>
                 <Slider
                   id="quality"
@@ -346,36 +361,184 @@ function PDFOptions({ onGenerate, onReset }: PDFOptionsProps) {
                 />
                 </div> */}
 
-          <div className="space-y-2">
-            <Label htmlFor="filename">Output Filename</Label>
-            <Input id="filename" placeholder="Enter filename" {...register("filename")} />
+            <div className="space-y-2">
+              <Label htmlFor="filename">Output Filename</Label>
+              <Input id="filename" placeholder="Enter filename" {...register("filename")} />
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-4 mt-6">
-          <Button type="button" variant="outline" className="flex-1" onClick={onReset}>
-            <RefreshCwIcon className="mr-2 h-4 w-4" /> Reset
-          </Button>
+          <div className="flex flex-col gap-4 mt-6">
+            <Button type="button" variant="outline" className="flex-1" onClick={onReset}>
+              <RefreshCwIcon className="mr-2 h-4 w-4" /> Reset
+            </Button>
 
-          <Button
-            className={cn("flex-1 flex gap-2 items-center", { "bg-primary/80": isSubmitting })}
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? (
-              <>
-                <LoaderCircle className="h-4 w-4 animate-spin" />
-                <span>Generating</span>
-              </>
-            ) : (
-              <>
-                <DownloadIcon className="h-4 w-4" />
-                <span>Generate PDF</span>
-              </>
-            )}
-          </Button>
-        </div>
-      </form>
-    </div>
+            <Button
+              className={cn("flex-1 flex gap-2 items-center", { "bg-primary/80": isSubmitting })}
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <>
+                  <LoaderCircle className="h-4 w-4 animate-spin" />
+                  <span>Generating</span>
+                </>
+              ) : (
+                <>
+                  <DownloadIcon className="h-4 w-4" />
+                  <span>Generate PDF</span>
+                </>
+              )}
+            </Button>
+          </div>
+        </form>
+      </div>
+
+      <DialogPrimitive.Root>
+        <DialogTrigger className="md:hidden w-14 h-14 grid place-items-center rounded-full bg-primary text-white fixed right-6 bottom-6 z-40 hover:bg-primary/90 transition-all">
+          <Settings2 className="w-6 h-6" />
+          <span className="sr-only">Open</span>
+        </DialogTrigger>
+
+        <DialogPrimitive.Portal container={container}>
+          <DialogPrimitive.Overlay className="absolute inset-0 z-50 bg-black/30" />
+          <DialogPrimitive.Content asChild>
+            <div className="w-full max-w-80 scrollbar-thin overflow-y-auto border border-l z-50 p-4 h-full shrink-0 bg-background md:flex flex-col absolute top-0 right-0 data-[state=open]:animate-in data-[state=closed]:animate-out slide-in-from-right slide-out-to-right">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col justify-between gap-4 grow"
+              >
+                <h2 className="text-lg font-semibold mb-6 sr-only">PDF Options</h2>
+
+                <DialogPrimitive.Close className="p-1 hover:bg-neutral-50 w-fit rounded-full ml-auto">
+                  <span className="sr-only">Close</span>
+                  <X className="w-4 h-4" />
+                </DialogPrimitive.Close>
+
+                <div className="flex flex-col gap-4 grow">
+                  <div className="space-y-2">
+                    <Label htmlFor="page-size">Page Size</Label>
+                    <Controller
+                      control={control}
+                      name="size"
+                      render={({ field }) => (
+                        <Select onValueChange={field.onChange} {...field}>
+                          <SelectTrigger id="page-size">
+                            <SelectValue placeholder="Select page size" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="a4">A4</SelectItem>
+                            <SelectItem value="letter">Letter</SelectItem>
+                            <SelectItem value="legal">Legal</SelectItem>
+                            <SelectItem value="fit">Fit (Same size as image)</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      )}
+                    />
+                  </div>
+
+                  <div className="space-y-2">
+                    <Label htmlFor="page-size">Margin</Label>
+
+                    <Controller
+                      control={control}
+                      name="margin"
+                      render={({ field }) => (
+                        <Select onValueChange={field.onChange} {...field}>
+                          <SelectTrigger id="page-size">
+                            <SelectValue placeholder="Select page margin" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="none">None</SelectItem>
+                            <SelectItem value="small">Small</SelectItem>
+                            <SelectItem value="normal">Normal</SelectItem>
+                            <SelectItem value="large">Big</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      )}
+                    />
+                  </div>
+
+                  <div className="space-y-2">
+                    <Label htmlFor="orientation">Orientation</Label>
+                    <Controller
+                      control={control}
+                      name="orientation"
+                      render={({ field }) => (
+                        <Select onValueChange={field.onChange} {...field}>
+                          <SelectTrigger id="orientation">
+                            <SelectValue placeholder="Select orientation" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="portrait">Portrait</SelectItem>
+                            <SelectItem value="landscape">Landscape</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      )}
+                    />
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <Label htmlFor="compress">Compress PDF</Label>
+                    <Controller
+                      control={control}
+                      name="compress"
+                      render={({ field }) => (
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                          id="compress"
+                        />
+                      )}
+                    />
+                  </div>
+
+                  {/* <div className="space-y-2">
+                <Label htmlFor="quality">Image Quality</Label>
+                <Slider
+                  id="quality"
+                  min={0}
+                  max={100}
+                  step={1}
+                  defaultValue={[80]}
+                  className="w-full"
+                />
+                </div> */}
+
+                  <div className="space-y-2">
+                    <Label htmlFor="filename">Output Filename</Label>
+                    <Input id="filename" placeholder="Enter filename" {...register("filename")} />
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-4 mt-6">
+                  <Button type="button" variant="outline" className="flex-1" onClick={onReset}>
+                    <RefreshCwIcon className="mr-2 h-4 w-4" /> Reset
+                  </Button>
+
+                  <Button
+                    className={cn("flex-1 flex gap-2 items-center", {
+                      "bg-primary/80": isSubmitting,
+                    })}
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? (
+                      <>
+                        <LoaderCircle className="h-4 w-4 animate-spin" />
+                        <span>Generating</span>
+                      </>
+                    ) : (
+                      <>
+                        <DownloadIcon className="h-4 w-4" />
+                        <span>Generate PDF</span>
+                      </>
+                    )}
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </DialogPrimitive.Content>
+        </DialogPrimitive.Portal>
+      </DialogPrimitive.Root>
+    </>
   );
 }
 
@@ -384,19 +547,12 @@ interface SelectImagesProps {
 }
 function SelectImages({ onSelect }: SelectImagesProps) {
   const [isDragOver, setIsDragOver] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const files = Array.from(e.target.files || []);
     if (files.length < 1) return;
 
-    setIsLoading(true);
-    alert()
-    setTimeout(() => {
-      onSelect(files);
-      e.target.value = "";
-      setIsLoading(false);
-    });
+    onSelect(files);
   };
 
   const handleDrop = (e: DragEvent<HTMLLabelElement>) => {
@@ -441,13 +597,9 @@ function SelectImages({ onSelect }: SelectImagesProps) {
           multiple
           accept="image/*"
         />
-        {isLoading ? (
-          <LoaderCircle width={50} height={50} className="animate-spin" />
-        ) : (
-          <ImageUp width={50} height={50} />
-        )}
+        <ImageUp width={50} height={50} />
         <p className="text-center font-medium">
-          {isLoading ? "Please wait..." : "Drag & drop images here or click to select images."}
+          Drag & drop images here or click to select images.
         </p>
       </label>
     </div>
